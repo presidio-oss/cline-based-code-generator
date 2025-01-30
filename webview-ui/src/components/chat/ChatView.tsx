4e41a7c6f82f2c11f70aca6c57a0a82942cbc59a
magesh-presidio <131435897+magesh-presidio@users.noreply.github.com>
2025-01-30 20:04:57 +0530

    fix: removed the unused variable (#37)

--- webview-ui/src/components/chat/ChatView.tsx
+++ webview-ui/src/components/chat/ChatView.tsx
@@ -48,7 +48,7 @@ const ChatView = ({
 	onTaskSelect,
 	selectedHaiTask,
 }: ChatViewProps) => {
-	const { version, clineMessages: messages, taskHistory, apiConfiguration } = useExtensionState()
+	const { clineMessages: messages, apiConfiguration } = useExtensionState()
 
 	//const task = messages.length > 0 ? (messages[0].say === "task" ? messages[0] : undefined) : undefined) : undefined
 	const task = useMemo(() => messages.at(0), [messages]) // leaving this less safe version here since if the first message is not a task, then the extension is in a bad state and needs to be debugged (see Cline.abort)
